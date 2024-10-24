66f73746865ff9b85bcd959a36b9acacb66c6508
whatacotton <whatacotton@gmail.com>
2024-10-25 00:47:23 +0900

    delete unexpected token

--- home/scripts/install-hm-nixos.sh
+++ home/scripts/install-hm-nixos.sh
@@ -3,4 +3,4 @@
 sudo chmod 777 /etc/nixos/home
 sudo ln -s /etc/nixos/home $HOME/.config/home-manager
 sudo ln -s /etc/nixos/home/config/zsh/oh-my-zsh $HOME/.config/oh-my-zsh
-nix run home-manager -- switch --flake .#myHomeConfig`
+nix run home-manager -- switch --flake .#myHomeConfig

f002e5408a647a96e3f4e28ca00066614bbdbef8
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-11-23 03:35:52 +0900

    feat: add ookla-speedtest

--- home/config/commands.nix
+++ home/config/commands.nix
@@ -15,10 +15,17 @@
     parted
     gparted
     zip
+    unzip
     rename
+    #tty-clock: https://zenn.dev/nukokoi/articles/539017fa274cf4
+    tty-clock
+    ookla-speedtest
     # ネタコマンド
     lolcat
     sl
     cowsay
+    cmatrix
+    bastet
+    oneko
   ];
 }

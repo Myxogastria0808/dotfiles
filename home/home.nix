d12b6d254651ba1c6a4bd6f86a5cf544c3065d48
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-12-26 00:24:33 +0900

    feat: remove file

--- home/home.nix
+++ home/home.nix
@@ -7,13 +7,6 @@
     username = "${username}";
     homeDirectory = "/home/${username}";
     stateVersion = "24.05";
-    file = {
-      "user.dict" = {
-        enable = true;
-        target = "/home/${username}/.local/share/fcitx5/skk/user.dict";
-        source = ./config/skk/user.dict;
-      };
-    };
   };
   # Enable home-manager
   programs.home-manager.enable = true;

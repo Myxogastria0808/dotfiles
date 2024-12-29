3a9f4e34954ef9c2cd18e5012d4ac8170da67a63
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-12-30 05:20:44 +0900

    feat: add zed-editor userSettings

--- home/config/zed-editor.nix
+++ home/config/zed-editor.nix
@@ -2,6 +2,14 @@
   programs.zed-editor = {
     enable = true;
     # extensions = [ ];
-    # userSettings = { };
+    userSettings = {
+      ui_font_size = 16;
+      buffer_font_size = 16;
+      theme = {
+        mode = "sysytem";
+        light = "One Light";
+        dark = "One Dark";
+      };
+    };
   };
 }

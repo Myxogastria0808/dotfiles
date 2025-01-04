ba271277861a746ff54943a1975b8d416b80bd90
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-01-04 16:18:11 +0900

    fix: remove comment out

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -203,6 +203,14 @@
   # Enable TailScale
   services.tailscale.enable = true;
 
+  # Enable Steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   virtualisation = {
     # Enable docker
     docker.enable = true;

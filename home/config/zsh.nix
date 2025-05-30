920cf237a4bc26b448b93a43b1cabcf4920cf4f3
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-05-30 11:57:18 +0900

    wip

--- home/config/zsh.nix
+++ home/config/zsh.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
+  home.packages = with pkgs; [
     # one of zsh plugins
     zsh-you-should-use
   ];
--- modules/config/yazi.nix
+++ modules/config/yazi.nix
@@ -1,6 +1,8 @@
 {
-  #Documentation: https://yazi-rs.github.io/docs/configuration/overview
+  # Documentation: https://yazi-rs.github.io/docs/configuration/overview
   programs.yazi = {
     enable = true;
+    # Documentation: https://yazi-rs.github.io/docs/configuration/yazi/
+    programs.yazi.settings.yazi = {};
   };
 }

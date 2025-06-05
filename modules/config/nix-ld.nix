ccd129366a0cc15017b13a4f45ddf30ab8e096dd
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-06-06 02:16:36 +0900

    fix: add imports

--- modules/config/nix-ld.nix
+++ modules/config/nix-ld.nix
@@ -1,10 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
-    # 共有ライブラリについて: https://qiita.com/syoshika_/items/435a2183265b23bc73fa
-    programs.nix-ld.enable = true;
-    programs.nix-ld.libraries = with pkgs; [
-        # required Air (R language formatter)
-        libgcc
-        glibc
-    ];
+  # 共有ライブラリについて: https://qiita.com/syoshika_/items/435a2183265b23bc73fa
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # required Air (R language formatter)
+    libgcc
+    glibc
+  ];
 }

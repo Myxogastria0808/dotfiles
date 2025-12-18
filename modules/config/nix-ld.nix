daa96e5ee29b30b94db1de489c629f7a1c8c9f27
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-12-19 01:25:32 +0900

    wip

--- modules/config/nix-ld.nix
+++ modules/config/nix-ld.nix
@@ -1,10 +1,12 @@
 { pkgs, ... }:
 {
   # What is shared library: https://qiita.com/syoshika_/items/435a2183265b23bc73fa
-  programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs; [
-    # required Air (R language formatter)
-    libgcc
-    glibc
-  ];
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      # required Air (R language formatter)
+      libgcc
+      glibc
+    ];
+  }
 }

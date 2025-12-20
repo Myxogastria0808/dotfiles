9d49b11ead80d42a1e7dad101c4ca32db5713de6
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-12-20 15:06:57 +0900

    chore: add some languages

--- modules/config/language.nix
+++ modules/config/language.nix
@@ -3,11 +3,16 @@
   environment.systemPackages = with pkgs; [
     # C/C++
     gcc
+    # Rust
+    cargo
+    # Golang
+    go
+    # Nodejs
+    nodejs
+    corepack
     # python
     python3
     # R
     R
-    # Golang
-    go
   ];
 }

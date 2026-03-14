1e93a67bbf3668967bc762ad52f4f2d160545969
Myxogastria0808 <r.rstudio.c@gmail.com>
2026-03-15 01:34:49 +0900

    wip

--- home/config/hyprland/waybar/default.nix
+++ home/config/hyprland/waybar/default.nix
@@ -89,9 +89,9 @@ in
           return-type = "json";
           interval = "once";
           signal = 1;
-          on-scroll-up = "pactl set-source-volume @DEFAULT_SOURCE@ +5% && pkill -RTMIN+1 waybar";
-          on-scroll-down = "pactl set-source-volume @DEFAULT_SOURCE@ -5% && pkill -RTMIN+1 waybar";
-          on-click = "pactl set-source-mute @DEFAULT_SOURCE@ toggle && pkill -RTMIN+1 waybar";
+          on-scroll-up = "pactl set-source-volume @DEFAULT_SOURCE@ +5% && kill -RTMIN+1 $(pgrep -x waybar 2>/dev/null || pgrep waybar)";
+          on-scroll-down = "pactl set-source-volume @DEFAULT_SOURCE@ -5% && kill -RTMIN+1 $(pgrep -x waybar 2>/dev/null || pgrep waybar)";
+          on-click = "pactl set-source-mute @DEFAULT_SOURCE@ toggle && sleep 0.1 && kill -RTMIN+1 $(pgrep -x waybar 2>/dev/null || pgrep waybar)";
           tooltip = false;
         };
 
